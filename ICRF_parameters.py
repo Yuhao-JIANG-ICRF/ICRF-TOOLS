3fbe2d0ff832275eab5eeb04bd1b23745792bf88
Yuhao-JIANG-ICRF <327201778@qq.com>
2025-03-03 00:10:01 +0100

    Add files via upload

---
+++ ICRF_parameters.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  1 00:39:04 2025
+
+@author: icrf
+"""
+'''
+Some important parameters
+'''
+import numpy as np
+
+c = 299792458           # light speed [m/s]
+e0 = 8.854187817e-12    #Vacuum Permittivity [F/m]
+u0 = 4*np.pi*1e-7       #permeability of vacuum [H/m]
+
+q0 = 1.60217733e-19;    #elementary positive charge [C]
+m0 = 1.6726231e-27;     #Atomic mass unit [kg]
+
+
--- Res_Freq.py
+++ Res_Freq.py
@@ -3,44 +3,107 @@
 """
 Created on Fri Feb 28 16:54:55 2025
 
-@author: YJ281217
+@author: Lara HIJAZI
 """
+globals().clear()
+get_ipython().magic('reset -f')
+get_ipython().magic('clear')
 
 import numpy as np
 import matplotlib.pyplot as plt
+import ICRF_parameters as ICp
+plt.close('all')     #close all figures
 
 # Constants
-B0 = 3.657  # Tesla
-N = 1        # Harmonic number
+#RB0 = 2.5;     #position of axis [m]
+R0 = 2.5;      #major radius [m]
+a  = 0.5;      #minor radius [m]
+
+B0 = 3.657;     #magnetic field [T]
+
+N = 2        # Harmonic number
 
 
-# Define range of R0 values (from 2m to 3m)
-R0_values = np.linspace(2.0, 3.0, 100)
-
+# Define range of R values (from R0-a to R0+a) 
+R = np.linspace(R0-a, R0+a, 200)
+if not np.isclose(R, R0).any():
+    R = np.sort(np.append(R, R0))
+    indices = np.where(R == R0)[0]
+if 'RB0' in globals() and RB0 != R0 and not np.isclose(R, RB0).any():
+    R = np.sort(np.append(R, RB0))
+    indices = np.where(R == RB0)[0]
+    
+    
 # Define the resonance frequency function
-def resonance_frequency(R0, Z, A):
-    return (15.225 * B0 * N * Z*2.5) / (A * (R0_values))  # MHz
+def resonance_frequency(Z, A):
+    return  N* 1/(2*np.pi*1e6) * (Z * ICp.q0)/(A * ICp.m0) * (B0*R0) / R# MHz
+
+
 
 # Compute f for different species
-f_H = resonance_frequency(R0_values, Z=1, A=1)   # Hydrogen (H)
-f_D = resonance_frequency(R0_values, Z=1, A=2)   # Deuterium (D)
-f_He = resonance_frequency(R0_values, Z=2, A=4)  # Helium (He)
-f_Li = resonance_frequency(R0_values, Z=3, A=7)  # Lithium (Li)
+f_H = resonance_frequency(Z=1, A=1)   # Hydrogen (H)
+f_D = resonance_frequency(Z=1, A=2)   # Deuterium (D)
+f_T = resonance_frequency(Z=1, A=3)  # Helium (He)
+f_He = resonance_frequency(Z=2, A=3)  # Helium (He)
+f_Li = resonance_frequency(Z=3, A=7)  # Lithium (Li)
 
 # Create the plot
 plt.figure(figsize=(7, 5))
-plt.plot(R0_values, f_H, label="H 1st & D 2nd", linewidth=2)
-plt.plot(R0_values, f_He, label="He", linewidth=2)
-plt.plot(R0_values, f_Li, label="Li", linewidth=2)
+plt.rcParams.update({
+    'lines.linewidth':2,
+    'xtick.direction':'in',
+    'ytick.direction':'in',
+    'xtick.top':True,
+    'xtick.bottom':True,
+    'ytick.left':True,
+    'ytick.right':True,
+    'axes.grid':True, 
+    'grid.linestyle': '--',  
+    'grid.alpha': 0.6  
+    })
+plt.plot(R, f_H, label="H")
+plt.plot(R, f_D, label="D")
+plt.plot(R, f_T, label="T")
+plt.plot(R, f_He, label="$^3$He")
+plt.plot(R, f_Li, label="$^7$Li")
 
+
+YL = plt.ylim()
+if 'RB0' in globals():
+    plt.plot([RB0, RB0],[YL[0], YL[1]],'k--' ,label="Magnetic Axis")
+    outline = f'At magnetic axis = {RB0} [m],'
+else:
+    plt.plot([R0, R0],[YL[0], YL[1]],'k--' ,label="Major Radius")
+    outline = f'At major radius = {R0} [m],'
+
+
+plt.xlim([R0-a, R0+a])
+plt.ylim([YL[0], YL[1]])
 # Labels and title
-plt.xlabel("R (m)", fontsize=12)
-plt.ylabel("f (MHz)", fontsize=12)
-plt.title("Resonance Frequency vs Major Radius", fontsize=14)
+plt.xlabel("Resonance Position [m]", fontsize=12)
+plt.ylabel("ICRF Frequency [MHz]", fontsize=12)
+if N==1:
+    plt.title("Fundamental Frequency vs Resonance Position", fontsize=14)
+    outline = outline + '\n\n Fundamental resonance frequency:'
+elif N==2:
+    plt.title("Second Harmonic Frequency vs Resonance Position", fontsize=14)
+    outline = outline + '\n\n second harmonic resonance frequency:'
 
 # Formatting grid and legend
-plt.grid(True, linestyle="--", alpha=0.6)
 plt.legend(fontsize=11)
 
 # Show plot
 plt.show()
+plt.plot(R, f_D, label="D")
+plt.plot(R, f_T, label="T")
+plt.plot(R, f_He, label="$^3$He")
+plt.plot(R, f_Li, label="$^7$Li")
+
+print(f'''
+      {outline}
+      H   = {f_H[indices].item():.1f} [MHz]
+      D   = {f_D[indices].item():.1f} [MHz]
+      T   = {f_T[indices].item():.1f} [MHz]
+      He3 = {f_He[indices].item():.1f} [MHz]
+      Li7 = {f_Li[indices].item():.1f} [MHz]
+      ''')
